72fe6d331ce8f80ea2f7cd3d75e23b9e4aa2361c
Dirk Weil <dirk.weil@gedoplan.de>
2016-12-02 10:04:53 +0100

    DB-Zugriff reaktiviert

--- src/main/java/de/gedoplan/micro/persistence/EntityManagerProducer.java
+++ src/main/java/de/gedoplan/micro/persistence/EntityManagerProducer.java
@@ -1,10 +1,13 @@
 package de.gedoplan.micro.persistence;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @ApplicationScoped
 public class EntityManagerProducer {
-  // @PersistenceContext(unitName = "showcase-ee")
-  // @Produces
-  // EntityManager entityManager;
+  @PersistenceContext(unitName = "showcase-ee")
+  @Produces
+  EntityManager entityManager;
 }

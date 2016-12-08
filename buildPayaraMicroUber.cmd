29ce2318ce9c024ba205536c44996dd0f6f2eec5
Dirk Weil <dirk.weil@gedoplan.de>
2016-12-08 15:13:34 +0100

    Payara Micro: Uber Jar bauen und starten

---
+++ buildPayaraMicroUber.cmd
@@ -0,0 +1,2 @@
+call buildPayaraMicro.cmd
+java -jar target\dependency\payara-micro-4.1.1.164.jar --domainConfig target\classes\microdomain-nocluster.xml --deploy target\micro-ohne-server.war --outputUberJar target\micro-ohne-server-uber.jar
